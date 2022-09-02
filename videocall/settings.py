3e1147448635331dd2b3689892032b837424c15d
BaktiyarJakshylykov <baktiarzaksylykov@gmail.com>
2022-09-02 13:56:45 +0600

    second deploy

--- videocall/settings.py
+++ videocall/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['videocall.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['video02.vercel.app', '.now.sh']
 
 # Application definition
 
